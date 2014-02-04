6d2061c4b3da5961e6e1658cf8495e821817f8a9
John Gruber <john.t.gruber@gmail.com>
2014-02-04 17:52:05 -0600

    whoops... removed my uneeded additions

--- agent/setup.py
+++ agent/setup.py
@@ -12,11 +12,8 @@ setup(name='f5-bigip-lbaas-agent',
                   'neutron.services.loadbalancer.drivers.f5.bigip.agent_api',
                   'neutron.services.loadbalancer.drivers.f5.bigip.agent_manager',
                   'neutron.services.loadbalancer.drivers.f5.bigip.icontrol_driver'],
-      packages=['f5', 'f5.common', 'f5.bigip', 'f5.bigip.bigip_interfaces', 'f5.bigip.pycontrol'],
+      packages=  ['f5', 'f5.common', 'f5.bigip', 'f5.bigip.bigip_interfaces', 'f5.bigip.pycontrol'],
       data_files=[('/usr/bin', ['usr/bin/f5-bigip-lbaas-agent']),
-                  ('/etc/neutron', ['etc/neutron/f5-bigip-lbaas-agent.ini']),
-                  ('/etc/init', ['etc/init/f5-bigip-lbaas-agent.conf'])],
-      scripts=['scripts/setup_symlink_upstart.py', 'scripts/setup_symlink_upstart.py']
+                  ('/etc/neutron', ['etc/neutron/f5-bigip-lbaas-agent.ini'])]
 
      )
-
