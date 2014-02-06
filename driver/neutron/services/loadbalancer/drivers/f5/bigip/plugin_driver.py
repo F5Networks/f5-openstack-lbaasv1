bf78fddd965e555547d1498a944a77d6668775f7
John Gruber <john.t.gruber@gmail.com>
2014-02-06 00:16:55 -0600

    testing viewing ml2 types

--- driver/neutron/services/loadbalancer/drivers/f5/bigip/plugin_driver.py
+++ driver/neutron/services/loadbalancer/drivers/f5/bigip/plugin_driver.py
@@ -448,6 +448,9 @@ class BigIPPluginDriver(abstract_driver.LoadBalancerAbstractDriver):
         # create an instance reference to the agent scheduler
         self.pool_scheduler = importutils.import_object(
             cfg.CONF.f5_loadbalancer_pool_scheduler_driver)
+        core_plugin = plugin._core_plugin
+        if hasattr(core_plugin, 'type_manager'):
+            LOG.debug('core types are %s' % core_plugin.type_manager.drivers)
 
     def get_pool_agent(self, context, pool_id):
         # define which agent to communicate with to handle provision
