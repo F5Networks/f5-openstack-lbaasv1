67c72a1bb3a4e7c1f5a479d7d1e70f7ba4fe16ed
John Gruber <john.t.gruber@gmail.com>
2014-02-19 04:41:01 -0600

    first commit with all the code for multiple devices and traffic groups - less some bugs <g>.

--- agent/f5/bigip/bigip_interfaces/__init__.py
+++ agent/f5/bigip/bigip_interfaces/__init__.py
@@ -48,7 +48,7 @@ def icontrol_folder(method):
                         kwargs[name] = instance.bigip.set_folder(kwargs[name],
                                                              folder)
 
-        instance.bigip.set_folder(None, kwargs['folder'])
+            instance.bigip.set_folder(None, kwargs['folder'])
 
         return method(*args, **kwargs)
     return wrapper
