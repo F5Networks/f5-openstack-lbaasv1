4c9a07eba8bc3fe5c95afe1e5f8bee091cd5e136
John Gruber <john.t.gruber@gmail.com>
2014-04-01 13:31:29 -0500

    cleaned up imports

--- agent/f5/bigip/bigip_interfaces/rule.py
+++ agent/f5/bigip/bigip_interfaces/rule.py
@@ -1,16 +1,7 @@
-from f5.common import constants as const
-from f5.bigip import exceptions
-from f5.bigip.bigip_interfaces import domain_address
+from f5.common.logger import Log
 from f5.bigip.bigip_interfaces import icontrol_folder
-from f5.bigip.bigip_interfaces import strip_folder_and_prefix
 
 from suds import WebFault
-import os
-import netaddr
-
-import logging
-
-LOG = logging.getLogger(__name__)
 
 
 class Rule(object):
@@ -35,8 +26,8 @@ class Rule(object):
                 return True
             except WebFault as wf:
                 if "already exists in partition" in str(wf.message):
-                    LOG.error(_(
-                        'tried to create a Rule when exists'))
+                    Log.error('Rule',
+                              'tried to create a Rule when exists')
                     return False
                 else:
                     raise wf
