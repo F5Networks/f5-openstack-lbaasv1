8c79335c6f68d14187574b410a205360498db4ea
John Gruber <john.t.gruber@gmail.com>
2014-04-01 13:29:31 -0500

    added tunnel constants

--- agent/neutron/services/loadbalancer/drivers/f5/bigip/constants.py
+++ agent/neutron/services/loadbalancer/drivers/f5/bigip/constants.py
@@ -1,1 +1,10 @@
-VALID_L2_TYPES = ['local', 'vlan']
+# Topic for tunnel notifications between the plugin and agent
+TUNNEL = 'tunnel'
+
+# Values for network_type
+TYPE_FLAT = 'flat'
+TYPE_VLAN = 'vlan'
+TYPE_GRE = 'gre'
+TYPE_LOCAL = 'local'
+TYPE_VXLAN = 'vxlan'
+VXLAN_UDP_PORT = 4789
