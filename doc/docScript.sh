c4e40672dc3c62eb226043187072a92c5c6a1dd8
Jodie Putrino <j.putrino@f5.com>
2015-10-27 14:16:50 -0600

    Fixes: jekyll build failing
    Problem:
    
    Analysis:
    - replaced 'bundle install...' with 'bundle exec'
    
    Unit/Robot tests:

--- doc/docScript.sh
+++ doc/docScript.sh
@@ -6,15 +6,13 @@
 #  Created by Jodie Putrino on 10/27/15.
 #
 
-bundle install pandoc-ruby
-
 FILES=*.md
 for f in $FILES
 do
 # extension="${f##*.}"
 filename="${f%.*}"
 echo "Converting $f to $filename.html"
-`pandoc $f -t html5 -o $filename.html`
+`bundle exec pandoc $f -t html5 -o $filename.html`
 # uncomment this line to delete the source file.
 # rm $f
 done
