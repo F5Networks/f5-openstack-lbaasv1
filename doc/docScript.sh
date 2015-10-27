ba1c5b6752a79b6abc118684c90d4d9916dbc9c4
Jodie Putrino <j.putrino@f5.com>
2015-10-27 15:47:25 -0600

    Fixes: travis build with pandoc still failing
    Problem: travis couldn't use pandoc
    
    Analysis: trying normal jekyll build instead
    
    Unit/Robot tests:

--- doc/docScript.sh
+++ doc/docScript.sh
@@ -1,28 +1,19 @@
 #!/usr/bin/env sh
 
-#  docScript2.sh
+#  docScript.sh
 #  
 #
 #  Created by Jodie Putrino on 10/27/15.
 #
 
 #install gems in Gemfile
-bundle install
+#bundle install
 
-#install Pandoc from source
-set -ex
-wget https://github.com/jgm/pandoc/archive/1.15.1.tar.gz
-tar -xzvf 1.15.1.tar.gz
-cd 1.15.1 && ./configure --prefix=/usr && make && sudo make install
 
 # run script to convert files
-FILES="*.md"
-for f in $FILES
-do
-# extension="${f##*.}"
-filename="${f%.*}"
-echo "Converting $f to $filename.html"
-pandoc "$f" -t html5 -o "$filename.html"
-# uncomment this line to delete the source file.
-# rm $f
-done
+bundle exec jekyll build -d ./f5-lbaasv1
+
+#echo "proofing site with htmlproofer"
+#bundle exec htmlproof ./site_build
+
+cp -R ./f5-lbaasv1 $HOME/f5-lbaasv1
