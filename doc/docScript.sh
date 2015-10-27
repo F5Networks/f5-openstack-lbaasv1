d0d9c2613a0723e45bc2d164d13284aadfb7e666
Jodie Putrino <j.putrino@f5.com>
2015-10-27 15:33:05 -0600

    Fixes: travis build still failing
    Problem:
    
    Analysis:
    - trying installing pandoc directly from source within the shell script
    
    Unit/Robot tests:

--- doc/docScript.sh
+++ doc/docScript.sh
@@ -6,15 +6,23 @@
 #  Created by Jodie Putrino on 10/27/15.
 #
 
+#install gems in Gemfile
 bundle install
 
+#install Pandoc from source
+set -ex
+wget https://github.com/jgm/pandoc/archive/1.15.1.tar.gz
+tar -xzvf pandoc-1.15.1.tar.gz
+cd pandoc-1.15.1 && ./configure --prefix=/usr && make && sudo make install
+
+# run script to convert files
 FILES="*.md"
 for f in $FILES
 do
 # extension="${f##*.}"
 filename="${f%.*}"
 echo "Converting $f to $filename.html"
-bundle exec pandoc "$f" -t html5 -o "$filename.html"
+pandoc "$f" -t html5 -o "$filename.html"
 # uncomment this line to delete the source file.
 # rm $f
 done
