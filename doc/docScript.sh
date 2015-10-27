717c2e68ee501cb3278799fef368b66e990323e1
Jodie Putrino <j.putrino@f5.com>
2015-10-27 14:24:28 -0600

    Fixes: travis build still failing
    Problem: script can't execute
    
    Analysis:
    - ran script through ShellCheck and made some corrections
    
    Unit/Robot tests:

--- doc/docScript.sh
+++ doc/docScript.sh
@@ -6,13 +6,13 @@
 #  Created by Jodie Putrino on 10/27/15.
 #
 
-FILES=*.md
+FILES="*.md"
 for f in $FILES
 do
 # extension="${f##*.}"
 filename="${f%.*}"
 echo "Converting $f to $filename.html"
-`bundle exec pandoc $f -t html5 -o $filename.html`
+bundle exec pandoc "$f" -t html5 -o "$filename.html"
 # uncomment this line to delete the source file.
 # rm $f
 done
