3a091e27b02332f6fcd5189ac511767603735479
Jodie Putrino <j.putrino@f5.com>
2015-11-19 10:35:42 -0700

    Fixes: travis build #54 failed
    Problem: wrong repo cloned by buildDocs.sh
    
    Analysis: changed repo name in script
    
    Unit/Robot tests:

--- script/buildDocs.sh
+++ script/buildDocs.sh
@@ -9,7 +9,7 @@
 
 # clone the website source repo
 
-git clone --verbose git@github.com:jputrino/openstack-f5-lbaasv1.git
+git clone --verbose git@github.com:jputrino/f5-openstack-docs.git
 
 mkdir f5-openstack-docs/f5-os-lbaasv1
 
