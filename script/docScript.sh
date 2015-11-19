8aa20000d3c0f48156fcaa6ec2baf185badd398b
Jodie Putrino <j.putrino@f5.com>
2015-11-19 10:55:32 -0700

    Fixes: website build / deploy issues
    Problem: I need to use the templates in the website repo to format the docs in the project repos.
    
    Analysis:
    - I changed .travis.yml to make "$HOME/f5-os-lbaas/doc the source repo for the deployment.
      Fixes: the doc subfolder was being deployed to s3.
    - I added '--verbose --trace' to the jekyll build command in docScript.sh.
    
    Unit/Robot tests:

--- script/docScript.sh
+++ script/docScript.sh
@@ -14,12 +14,12 @@
 
 
 # copy content of doc directory into project folder
-echo "copying doc directory into f5-os-lbaasv1"
-cp -R ./$TRAVISREPOSLUG/doc f5-openstack-docs/f5-os-lbaasv1
+echo "copying $TRAVISREPOSLUG/doc into f5-openstack-docs/"
+cp -R ./$TRAVISREPOSLUG/doc/ f5-openstack-docs/f5-os-lbaasv1
 
 # build site
 echo "building site with jekyll"
-bundle exec jekyll build --config _lbaasconfig.yml -s f5-openstack-docs -d ./site_build
+bundle exec jekyll build --verbose --trace --config _lbaasconfig.yml -s f5-openstack-docs -d ./site_build
 
 #echo "proofing site with htmlproofer"
 #bundle exec htmlproof ./site_build
@@ -28,5 +28,5 @@ echo "copying docs to $HOME"
 cp -R ./site_build/f5-os-lbaasv1 $HOME/f5-os-lbaasv1
 
 echo "listing contents of $HOME/f5-os-lbaasv1"
-ls -a $HOME/f5-os-lbaasv1
+ls -a $HOME/f5-os-lbaasv1/doc
 
