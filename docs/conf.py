5418caee6738ba9c737388e5f59271f6e442a7a3
Jodie Putrino <j.putrino@f5.com>
2016-05-02 13:08:44 -0600

    Fixes: N/A
    Problem: sphinx is using the wrong version of six by default, which is breaking the sphinx-build.
    
    Analysis: I added the current version of six to the docs requirements file.
    
    Unit/Robot tests:
    
    add heat, lbaasv2, & docs intersphinx mapping
    
    replace release with variable; replace compatibility table with intersphinx ref to releases and support matrix

--- docs/conf.py
+++ docs/conf.py
@@ -300,3 +300,11 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+intersphinx_mapping = {'heat': (
+     'http://f5-openstack-heat.readthedocs.org/en/latest', None),
+     'docs': (
+     'http://f5-openstack-docs.readthedocs.org/en/latest', None),
+     'lbaasv2': (
+     'http://f5-openstack-lbaasv2.readthedocs.org/en/latest', None),
+     }
